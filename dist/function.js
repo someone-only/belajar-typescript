55abe352cc2d8cb95ee0eab241db56419ea4d534
someone-only <akumemailanda@gmail.com>
2023-07-07 09:46:57 +0700

    menambahkan file function.ts

---
+++ dist/function.js
@@ -0,0 +1,9 @@
+"use strict";
+function printHello(nama) {
+    console.log(`hello ${nama}`);
+}
+printHello("dimas");
+function makanan(nama) {
+    return nama;
+}
+makanan("bubur ayam");
---
+++ src/function.ts
@@ -0,0 +1,11 @@
+function printHello(nama: string): void {
+        console.log(`hello ${nama}`)
+}
+
+printHello("dimas")
+
+function makanan(nama: string): string {
+        return nama
+}
+
+makanan("bubur ayam")
